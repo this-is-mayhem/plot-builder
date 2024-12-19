c058050f9a3f82ac2d162c16ec9f57e04c574acc
Головатюк Александр Сергеевич <sashromtool@gmail.com>
2024-12-19 11:49:23 +0300

    стили линий

--- csv_plotter.py
+++ csv_plotter.py
@@ -24,46 +24,57 @@ class csv_plotter:
     def plot_data(self):
         for file_config in self.config:
             file_name = file_config["name"]
-            axis_labels = file_config["axis"]["labels"]
-            axis_numbers_precision = file_config["axis"]["precision"]
-            single_x_axis = file_config["axis"]["single x axis"]
-            x_lim = file_config["axis"]["x limits"]
-            y_lim = file_config["axis"]["y limits"]
-            legend = file_config["legend"]
+
+            # обработка информации об осях
+            axes_labels = file_config["axes"]["labels"]
+            axes_precision = file_config["axes"]["precision"]
+            single_x_axis = file_config["axes"]["single x axis"]
+            x_lim = file_config["axes"]["x limits"]
+            y_lim = file_config["axes"]["y limits"]
+
+            # обработка информации о кривых графика
+            legend = file_config["charts"]["legend"]
+            style = file_config["charts"]["style"]
+
+            # обработка прочей информации
             figure_size = file_config["figure size"]
             figure_dpi = file_config["dpi"]
 
-
             # Считывание CSV файла
             file_path = os.path.join(self.dir, file_name)
-            data = pd.read_csv(file_path)
+            data = pd.read_csv(file_path, sep=";")
 
-            cm = 1 / 2.54  # centimeters in inches
+            cm = 1 / 2.54  # перевод сантиметров в дюймы для установки размера картинки
             plt.figure(figsize=(figure_size[0] * cm, figure_size[1] * cm))
 
             if data.shape[1] == 2:
                 # Для файлов с двумя столбцами: X и Y
-                #plt.plot(data.iloc[:, 0], data.iloc[:, 1], label=f"{legend[0]}")
                 plt.plot(data.iloc[:, 0], data.iloc[:, 1])
             elif single_x_axis:
                 # Общая ось X для Y1, Y2
                 for i in range(1, data.shape[1]):
-                    plt.plot(data.iloc[:, 0], data.iloc[:, i], label=f"{legend[i - 1]}")
+                    plt.plot(data.iloc[:, 0], data.iloc[:, i],
+                             label=f"{legend[i - 1]}",
+                             linestyle=style[i // 2][0],
+                             marker=style[i // 2][1])
             elif not single_x_axis:
                 # Разные оси X для каждой пары (X, Y)
                 for i in range(0, data.shape[1], 2):
-                    plt.plot(data.iloc[:, i], data.iloc[:, i + 1], label=f"{legend[i // 2]}")
+                    plt.plot(data.iloc[:, i], data.iloc[:, i + 1],
+                             label=f"{legend[i // 2]}",
+                             linestyle=style[i // 2][0],
+                             marker=style[i // 2][1])
 
             # Настройка осей и легенды
             plt.gca().set_xlim(x_lim)
             plt.gca().set_ylim(y_lim)
 
-            plt.xlabel(f"{axis_labels[0]}", fontsize=14)
-            plt.ylabel(f"{axis_labels[1]}", fontsize=14)
+            plt.xlabel(f"{axes_labels[0]}", fontsize=14)
+            plt.ylabel(f"{axes_labels[1]}", fontsize=14)
             plt.tick_params(axis="both", which="major", labelsize=14)
 
-            plt.gca().xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: format_x_ticker(x, pos, axis_numbers_precision[0])))
-            plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, pos: format_y_ticker(y, pos, axis_numbers_precision[1])))
+            plt.gca().xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: format_x_ticker(x, pos, axes_precision[0])))
+            plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, pos: format_y_ticker(y, pos, axes_precision[1])))
 
             if data.shape[1] != 2:
                 plt.legend(fontsize=12)
